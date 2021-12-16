728911423423bba396fb3c329bc8efe41c00d81c
DmitryBaev <trane.rst@gmail.com>
2021-12-16 20:25:24 +0200

    final version

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      console.log("success");
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      console.log("wrong");
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      }, 200);
+      $("h1").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+}
 
 function playSound(color) {
   let audio = new Audio(`sounds/${color}.mp3`);
@@ -10,13 +37,16 @@ function playSound(color) {
 
 function animatePress(currentColor) {
   $(`#${currentColor}`).addClass("pressed");
-  
+
   setTimeout(function() {
     $(`#${currentColor}`).removeClass("pressed");
-  }, 100)
+  }, 100);
 }
 
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("h1").text(`Level ${level}`);
   let randNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randNumber];
   gamePattern.push(randomChosenColour);
@@ -29,9 +59,11 @@ $(".btn").on("click", function(){
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 })
 
-// $("document").on("click", function() {
-//   nextSequence();
-//   console.log(gamePattern);
-// })
+$(document).on("keydown", function() {
+  if (level == 0) {
+    nextSequence();
+  }
+})
